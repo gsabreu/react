152ec1dd54fa6aca3864266aab3b7fd2ef01bffc
Guilherme Abreu <sabreuguilherme@gmail.com>
2021-11-15 18:03:35 -0300

    Adding flexbox concepts

--- udemy-coder/fundamentals-react/src/App.jsx
+++ udemy-coder/fundamentals-react/src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css"
 import React from "react";
 
 import First from "./components/basics/First";
@@ -8,25 +9,30 @@ import Card from "./components/layout/Card";
 
 export default () => {
     return (
-        <div id="app">
+        <div id="app" className="App">
             <h1>Fundamentals React</h1>
 
-            <Card title="#04 - Random Exercise">
-                <RandomNumber min={1} max={60}/>
-            </Card>
+            <div className="Cards">
+                <Card title="#04 - Random Exercise">
+                    <RandomNumber min={1} max={60} />
+                </Card>
 
-            <Card title="#03 - Fragment">
-                <Fragment />
-            </Card>
+                <Card title="#03 - Fragment">
+                    <Fragment />
+                </Card>
 
-            <Card title="#02 - With Param">
-                <WithParam titulo="Segundo Component" aluno="Pedro" nota={9.3} />
-            </Card>
+                <Card title="#02 - With Param">
+                    <WithParam
+                        titulo="Segundo Component"
+                        aluno="Pedro"
+                        nota={9.3}
+                    />
+                </Card>
 
-            <Card title="#01 - First Component">
-                <First></First>
-            </Card>
-            
+                <Card title="#01 - First Component">
+                    <First></First>
+                </Card>
+            </div>
         </div>
     );
 };
