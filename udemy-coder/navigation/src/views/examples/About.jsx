0fdc6f8be06aa3c6cf537fe3bf2ca570e39bf338
Guilherme Abreu <sabreuguilherme@gmail.com>
2022-01-06 11:55:24 -0500

    Learning React Router

---
+++ udemy-coder/navigation/src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = props => (
+    <div className="About">
+        <h1>Component About</h1>
+        <h2>About your system</h2>
+    </div>
+)
+
+export default About
